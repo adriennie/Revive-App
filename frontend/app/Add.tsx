64316cb24cfa7fe06de882c784e17986e22d258c
Adrien <115367800+adriennie@users.noreply.github.com>
2025-06-21 13:17:26 +0530

    Update Add.tsx

--- frontend/app/Add.tsx
+++ frontend/app/Add.tsx
@@ -2,6 +2,6 @@
 import React from 'react';
 import AddScreen from './screens/AddScreen';
 
-export default function Add() { // Export the component with a clear name
+export default function Add() {
   return <AddScreen />;
 }
