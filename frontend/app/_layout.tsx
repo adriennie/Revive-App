bd1c3ff6ba1df59aa09ea415c515cde4b8021bad
Jagrat Singh <jagratsingh9899@gmail.com>
2025-06-21 08:12:52 +0530

    changed some routing

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,4 +1,5 @@
 // app/_layout.tsx
+
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
@@ -9,13 +10,14 @@ export default function RootLayout() {
     <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <SafeAreaProvider>
         <StatusBar style="auto" />
+        
         <Stack
           screenOptions={{
             headerShown: false, // Hide header for all screens
             animation: 'slide_from_right', // Smooth transitions
           }}
         >
-          <Stack.Screen name="LandingPage" options={{ title: 'Welcome' }} />
+          <Stack.Screen name="index" options={{ title: 'Welcome' }} />
           <Stack.Screen name="(auth)/login" options={{ title: 'Login' }} />
           <Stack.Screen name="(auth)/sign-up" options={{ title: 'Sign Up' }} />
           <Stack.Screen name="home/index" options={{ title: 'Home' }} />
