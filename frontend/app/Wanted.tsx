8ce3f490b021d664d553f691cb56bf88a86b80cc
Adrien <115367800+adriennie@users.noreply.github.com>
2025-06-22 21:23:44 +0530

    Delete frontend/app/Wanted.tsx

--- frontend/app/Wanted.tsx
+++
@@ -1,7 +0,0 @@
-// app/Wanted.tsx
-import React from 'react';
-import WantedScreen from './screens/WantedScreen'; // You'll need to create app/screens/WantedScreen.tsx
-
-export default function Wanted() {
-  return <WantedScreen />;
-}
